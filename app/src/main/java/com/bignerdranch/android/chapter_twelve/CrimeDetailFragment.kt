5e987253ce19251cde8f58720fb0490fb97ab152
emmat <emma.trowbridgee@gmail.com>
2024-11-17 18:57:46 -0600

    Commit - Final Works V1

--- app/src/main/java/com/bignerdranch/android/chapter_twelve/CrimeDetailFragment.kt
+++ app/src/main/java/com/bignerdranch/android/chapter_twelve/CrimeDetailFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.core.widget.doOnTextChanged
 import com.bignerdranch.android.chapter_twelve.databinding.FragmentCrimeDetailBinding
 import java.util.Date
@@ -26,7 +27,6 @@ class CrimeDetailFragment : Fragment() {
             isSolved = false
 
         )
-
     }
 
     override fun onCreateView(
--- app/src/main/java/com/bignerdranch/android/chapter_twelve/database/CrimeDatabase.kt
+++ app/src/main/java/com/bignerdranch/android/chapter_twelve/database/CrimeDatabase.kt
@@ -7,10 +7,17 @@ import com.bignerdranch.android.chapter_twelve.Crime
 import androidx.room.Database as Database1
 
 
-@Database1(entities = [Crime:: class], version=1)
+/*@Database1(entities = [Crime:: class], version=1)
 @TypeConverters(CrimeTypeConverters::class)
 abstract class CrimeDatabase: RoomDatabase() {
     abstract fun crimeDAO(): CrimeDao
 
 
+}*/
+
+
+@Database1(entities = [Crime::class], version = 1, exportSchema = false)
+@TypeConverters(CrimeTypeConverters::class)
+abstract class CrimeDatabase : RoomDatabase() {
+    abstract fun crimeDAO(): CrimeDao
 }
